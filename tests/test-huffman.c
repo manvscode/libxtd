9e2347f5efc952f22652df7d6aed9b8008a44357
Joe Marrero <manvscode@gmail.com>
2013-09-20 15:34:21 -0400

    Updated license and README.

--- tests/test-huffman.c
+++ tests/test-huffman.c
@@ -1,16 +1,16 @@
 /*
- * Copyright (C) 2010 by Joseph A. Marrero and Shrewd LLC. http://www.manvscode.com/
- * 
+ * Copyright (C) 2010 by Joseph A. Marrero. http://www.manvscode.com/
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -30,7 +30,7 @@
 
 int main( int argc, char *argv[] )
 {
-	const char* restrict moby_dick = 
+	const char* restrict moby_dick =
 		"Call me Ishmael. Some years ago- never mind how long precisely- having little or no money in my purse, "
 		"and nothing particular to interest me on shore, I thought I would sail about a little and see the watery "
 		"part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever "
@@ -91,7 +91,7 @@ int main( int argc, char *argv[] )
 			printf( "%02x", (uint8_t) original[ i ] );
 		}
 		printf( " (%ld bytes)\n", original_size );
-	}	
+	}
 
 	free( compressed );
 	free( original );
--- tests/test-scrambling.c
+++ tests/test-scrambling.c
@@ -1,16 +1,16 @@
 /*
- * Copyright (C) 2010 by Joseph A. Marrero and Shrewd LLC. http://www.manvscode.com/
- * 
+ * Copyright (C) 2010 by Joseph A. Marrero. http://www.manvscode.com/
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -42,12 +42,12 @@ int main( int argc, char *argv[] )
 
 	text[ sizeof(text) - 1 ] = '\0';
 
-	unsigned short pivot = rand();	
-	
+	unsigned short pivot = rand();
+
 
 	scramble_string( SEED, text, len, pivot );
 	text[ sizeof(text) - 1 ] = '\0';
-	
+
 	print_divider( stdout, "[ Scrambling Text Test ]" );
 	printf( "      Pivot = [%d]\n", pivot );
 	printf( "       SEED = [%s]\n", SEED );
@@ -69,6 +69,6 @@ int main( int argc, char *argv[] )
 		printf( "%02x", text[i] );
 	}
 	printf( "]\n" );
-	
+
 	return 0;
 }
